a0505140309b90a76c89b0808d7ec088e0bcee4a
torrentuser <2207883832@qq.com>
2025-02-24 13:09:29 +0800

    upgrade:add copyrights

--- lib/widget/libraries/small_tag.dart
+++ lib/widget/libraries/small_tag.dart
@@ -14,6 +14,7 @@
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 import 'package:flutter/material.dart';
 
 /// SmallTag is a small tag, usually showing on the home page to visualize different data items.
