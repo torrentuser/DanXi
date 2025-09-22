f873ff272ca1789a29961684398403dd94080fd3
torrentuser <111721462+torrentuser@users.noreply.github.com>
2025-09-22 02:27:45 +0000

    Deploying to gh-pages from @ torrentuser/DanXi@9f63aa3776a3cdd487eb313ddf9942afd41784ef 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,7 +18,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "favicon.webp": "5d8a19c576a1ba2a24af3b99f340237d",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "manifest.json": "012d91b9edf492757ddf24dd1b15d03e",
-"main.dart.js": "0cc0190b00a0d6b48a4bd8964472cf63",
+"main.dart.js": "27ad575442725b62604863f5b6f39121",
 "version.json": "3ac35de81d647f650b6f3a674887919c",
 "assets/NOTICES": "715996fc7119ca474fbd3f00c6913814",
 "assets/fonts/MaterialIcons-Regular.otf": "4b3e2c198761cc0ff43b22148efde931",
@@ -71,9 +71,9 @@ const RESOURCES = {"canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_SansSerif-Bold.ttf": "ad0a28f28f736cf4c121bcb0e719b88a",
 "assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Fraktur-Regular.ttf": "dede6f2c7dad4402fa205644391b3a94",
 "assets/packages/flutter_inappwebview_web/assets/web/web_support.js": "509ae636cfdd93e49b5a6eaf0f06d79f",
-"flutter_bootstrap.js": "f441ec5989fa22c311a1e810e4505b54",
-"index.html": "e36191f153eedd6c196b62eb9963deab",
-"/": "e36191f153eedd6c196b62eb9963deab"};
+"flutter_bootstrap.js": "008b5485c04d8d0c975fee60f0b728ed",
+"index.html": "44fe52c1627985e66666cb22a553dfd1",
+"/": "44fe52c1627985e66666cb22a553dfd1"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
