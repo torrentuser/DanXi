43870dc0bf8a56d4562b4fc5b394db5449f125d3
torrentuser <111721462+torrentuser@users.noreply.github.com>
2026-03-04 11:06:30 +0000

    Deploying to gh-pages from @ torrentuser/DanXi@3220059af8a99aacc76090e2f8975a76bdac2a69 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3654917105" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2654596149" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
